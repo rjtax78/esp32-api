bc4a3cbae377de3d8d7d962778b52abbd46c00a9
Jean Tahina <143861785+rjtax78@users.noreply.github.com>
2024-12-14 12:27:12 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ import pg from 'pg'
 const Pool = pg.Pool
 
 const connectDb = new Pool({
-  user: 'postgres',
-  host: 'db.ekaferyxhpjbxgfqpgdu.supabase.co',  // Adresse IPv4 au lieu d'IPv6
-  database: 'postgres',
-  password: 'esp32-node',
+  user: "postgres",
+  host: "db.ekaferyxhpjbxgfqpgdu.supabase.co",
+  database: "postgres",
+  password: "esp32-node",
   port: 5432,
 });
 export default connectDb
